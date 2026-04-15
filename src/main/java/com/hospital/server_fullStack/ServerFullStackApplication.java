d7fc8f84d7db7c774f4954479a18f6c056a4b950
Nandhini M <nandhinimm4706@gmail.com>
2026-04-15 21:02:42 +0530

    Initial commit

---
+++ src/main/java/com/hospital/server_fullStack/ServerFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.server_fullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/server_fullStack/ServerFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.server_fullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
